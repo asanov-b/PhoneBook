2a9c990f8ad1aeb563820c261ba474da6999964c
Bekzod Asanov <Bekzodasanov07@gmail.com>
2026-01-26 11:16:24 +0500

    fix: small bug fixes

--- src/main/java/com/bekzod/phonebook/controller/AuthController.java
+++ src/main/java/com/bekzod/phonebook/controller/AuthController.java
@@ -4,6 +4,8 @@ import com.bekzod.phonebook.dto.LoginDTO;
 import com.bekzod.phonebook.dto.LoginResDTO;
 import com.bekzod.phonebook.dto.RegisterDTO;
 import com.bekzod.phonebook.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -24,14 +26,14 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping
-    public ResponseEntity<LoginResDTO> auth(@RequestBody LoginDTO loginDTO) {
+    public ResponseEntity<LoginResDTO> auth(@Valid @RequestBody LoginDTO loginDTO) {
         log.info("POST {} attempt for username={}", API_PATH + API_VERSION + AUTH, loginDTO.getUsername());
         LoginResDTO login = authService.login(loginDTO);
         return ResponseEntity.ok(login);
     }
 
     @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody RegisterDTO registerDTO) {
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterDTO registerDTO) {
         log.info("POST {}/register attempt for username={}", API_PATH + API_VERSION + AUTH, registerDTO.getUsername());
         authService.register(registerDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
